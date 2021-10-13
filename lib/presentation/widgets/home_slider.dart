4ce80416eed78890064d30e6ea27bafb3d2bdc67
MuhammedSafaroff <seferovmehemmed0@gmail.com>
2021-10-13 11:15:35 +0400

    carousel slider

--- lib/presentation/widgets/home_slider.dart
+++ lib/presentation/widgets/home_slider.dart
@@ -1,35 +1,69 @@
-import 'package:carousel_pro/carousel_pro.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:vstore_appl/core/constants/statics.dart';
 
 class HomeSlider extends StatelessWidget {
-  const HomeSlider({Key? key}) : super(key: key);
+  HomeSlider({Key? key}) : super(key: key);
+  List<String> images = [
+    'assets/images/slide1.png',
+    'assets/images/slide2.png',
+    'assets/images/slide3.png',
+    'assets/images/slide4.jpg',
+    'assets/images/slide5.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 180,
-      child: Carousel(
-        autoplay: false,
-        animationCurve: Curves.fastOutSlowIn,
-        animationDuration: Duration(milliseconds: 1200),
-        dotSize: 5.0,
-        dotIncreasedColor: backgroundColor,
-        dotBgColor: Colors.transparent,
-        dotVerticalPadding: 5.0,
-        dotSpacing: 18,
-        showIndicator: true,
-        indicatorBgPadding: 2.0,
-        boxFit: BoxFit.fill,
-        images: [
-          ExactAssetImage('assets/images/slide1.png'),
-          ExactAssetImage('assets/images/slide2.png'),
-          ExactAssetImage('assets/images/slide3.png'),
-          ExactAssetImage('assets/images/slide4.jpg'),
-          ExactAssetImage('assets/images/slide5.png'),
-        ],
+    return CarouselSlider(
+      options: CarouselOptions(
+        aspectRatio: 16 / 8,
+        viewportFraction: 1,
+        initialPage: 0,
+        enableInfiniteScroll: true,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 3),
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enlargeCenterPage: true,
+        scrollDirection: Axis.horizontal,
       ),
+      items: images.map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(i),
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
     );
   }
 }
+
+
+
+
+
+// Carousel(
+//         autoplay: false,
+//         animationCurve: Curves.fastOutSlowIn,
+//         animationDuration: Duration(milliseconds: 1200),
+//         dotSize: 5.0,
+//         dotIncreasedColor: backgroundColor,
+//         dotBgColor: Colors.transparent,
+//         dotVerticalPadding: 5.0,
+//         dotSpacing: 18,
+//         showIndicator: true,
+//         indicatorBgPadding: 2.0,
+//         boxFit: BoxFit.fill,
+//         images: [
+          
+//         ],
+//       ),
